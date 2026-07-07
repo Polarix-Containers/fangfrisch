cd0da26c2fbcf7e5f5f9557b1c00bca1fba2de9f
Tommy <contact@tommytran.io>
2026-07-07 13:24:02 -0700

    Update Fangfrisch path
    Signed-off-by: Tommy <contact@tommytran.io>

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 while ${INITIALIZED}; do
-    /app/fangfrisch --conf /etc/fangfrisch.conf refresh
+    /usr/local/bin/fangfrisch --conf /etc/fangfrisch.conf refresh
     sleep "${INTERVAL}"
 done
 
 # shellcheck disable=SC2006
-echo "Initialize your database with `/app/fangfrisch initdb` and set INITIALIZED=true"
+echo "Initialize your database with `fangfrisch initdb` and set INITIALIZED=true"
 sleep infinity
