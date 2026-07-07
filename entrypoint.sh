a46eb41efa4a2ec8693a26b020001310683c46da
Tommy <contact@tommytran.io>
2026-07-06 21:57:34 -0700

    Fix Shellcheck issues
    Signed-off-by: Tommy <contact@tommytran.io>

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,9 @@
 
 while ${INITIALIZED}; do
     /app/fangfrisch --conf /etc/fangfrisch.conf refresh
-    sleep ${INTERVAL}
+    sleep "${INTERVAL}"
 done
 
+# shellcheck disable=SC2006
 echo "Initialize your database with `/app/fangfrisch initdb` and set INITIALIZED=true"
 sleep infinity
